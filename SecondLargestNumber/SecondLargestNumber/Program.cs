0d21093477fa1cfd19247a0638fe0791583edb07
Avezz78 <113528051+Avezz78@users.noreply.github.com>
2022-09-16 22:13:43 +0530

    Add files via upload

---
+++ SecondLargestNumber/SecondLargestNumber/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SecondLargestNumber
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+           
+
+            int[] num = { 20, 10, 30, 50, 40 };//taking an unsorted array
+            int n=num.Length;//finding its length
+
+            //sort the array
+            Array.Sort(num);//sorting using sort method
+            Console.WriteLine("second largest number is " + num[n-2]);
+        }
+    }
+}
+    
+
